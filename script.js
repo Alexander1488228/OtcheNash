daa9d38dd627e715f477b6f3fa7cc1c1b4b90c2f
Alexander1488228 <sashaglovyuk@yandex.ru>
2025-06-30 00:41:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+    const status = document.getElementById("formStatus");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        status.textContent = "Сообщение отправлено!";
+        status.style.color = "green";
+        form.reset();
+    });
+});
